884ee947401b595c4e31b500d2713dfd2689fb79
baseboy1102002 <trannguyenvietthai@gmail.com>
2023-05-22 22:01:59 +0700

    final ver 1

---
+++ src/DTO/ReceivedProduct_DTO.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author DLCH
+ */
+public class ReceivedProduct_DTO {
+    String productID, productName, size;
+    double price;
+    int quantity;
+
+    public ReceivedProduct_DTO(String productID, String productName, String size, double price, int quantity) {
+        this.productID = productID;
+        this.productName = productName;
+        this.size = size;
+        this.price = price;
+        this.quantity = quantity;
+        
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    
+}
